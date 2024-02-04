44d0270240a97565f662caa458011e7f6f260709
mulirico <murillocontas@hotmail.com>
2024-02-04 17:28:09 -0300

    resetando algumas rotas para os testes funcionar

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,10 @@
 import pytest
 from fastapi.testclient import TestClient
+
 from fast_zero.app import app
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
 
 from fast_zero.models import Base
 
@@ -14,8 +16,15 @@ def client():
 
 @pytest.fixture
 def session():
-    engine = create_engine('sqlite:///:memory:')
-    Session = sessionmaker(bind=engine)
+    engine = create_engine(
+        'sqlite:///:memory:',
+        connect_args={'check_same_thread': False},
+        poolclass=StaticPool,
+    )
     Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+
     yield Session()
+
     Base.metadata.drop_all(engine)
