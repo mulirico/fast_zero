fedf37568809e5148c4ab1e4abc1c1d70d588d26
mulirico <murillocontas@hotmail.com>
2024-02-06 21:02:28 -0300

    Atualizando endpoints para usar o banco de dados real

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,24 @@
 import pytest
 from fastapi.testclient import TestClient
-
-from fast_zero.app import app
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import StaticPool
 
-from fast_zero.models import Base
+from fast_zero.app import app
+from fast_zero.database import get_session
+from fast_zero.models import Base, User
 
 
 @pytest.fixture
-def client():
-    return TestClient(app)
+def client(session):
+    def get_session_override():
+        return session
+
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
+        yield client
+
+    app.dependency_overrides.clear()
 
 
 @pytest.fixture
@@ -28,3 +35,13 @@ def session():
     yield Session()
 
     Base.metadata.drop_all(engine)
+
+
+@pytest.fixture
+def user(session):
+    user = User(username='Teste', email='teste@test.com', password='testtest')
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+
+    return user
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,6 @@
+from fast_zero.schemas import UserPublic
+
+
 def test_root_deve_retornar_200_e_ola_mundo(client):
     response = client.get('/')
 
@@ -22,21 +25,33 @@ def test_create_user(client):
     }
 
 
+def test_create_username_registered(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.post(
+        '/users/',
+        json={
+            'username': user_schema['username'],
+            'email': 'teste@example.com',
+            'password': 'testando',
+        },
+    )
+    assert response.status_code == 400
+    assert response.json() == {'detail': 'Username already registered'}
+
+
 def test_read_users(client):
-    response = client.get('/users/')
+    response = client.get('/users')
     assert response.status_code == 200
-    assert response.json() == {
-        'users': [
-            {
-                'username': 'alice',
-                'email': 'alice@example.com',
-                'id': 1,
-            }
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_update_user(client):
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users')
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
@@ -53,8 +68,27 @@ def test_update_user(client):
     }
 
 
-def test_delete_user(client):
-    response = client.delete('/users/1')
+def test_update_user_inexistent(client):
+    response = client.put(
+        '/users/2',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'User not found'}
 
+
+def test_delete_user(client, user):
+    response = client.delete('/users/1')
     assert response.status_code == 200
-    assert response.json() == {'detail': 'User deleted'}
+    assert response.json() == {'message': 'User deleted'}
+
+
+def test_delete_user_inexistent(client):
+    response = client.delete('/users/2')
+
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'User not found'}
