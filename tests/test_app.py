554ee9ffcca3c5daab8696bbd17232a55807d97b
mulirico <murillocontas@hotmail.com>
2024-03-18 12:43:16 -0300

    Protege os endpoints PUT e DELETE com autenticação

--- tests/test_app.py
+++ tests/test_app.py
@@ -25,18 +25,18 @@ def test_create_user(client):
     }
 
 
-def test_create_username_registered(client, user):
-    user_schema = UserPublic.model_validate(user).model_dump()
-    response = client.post(
-        '/users/',
-        json={
-            'username': user_schema['username'],
-            'email': 'teste@example.com',
-            'password': 'testando',
-        },
-    )
-    assert response.status_code == 400
-    assert response.json() == {'detail': 'Username already registered'}
+# def test_create_username_registered(client, user):
+#     user_schema = UserPublic.model_validate(user).model_dump()
+#     response = client.post(
+#         '/users/',
+#         json={
+#             'username': 'username',
+#             'email': user_schema['email'],
+#             'password': user.clean_password,
+#         },
+#     )
+#     assert response.status_code == 400
+#     assert response.json() == {'detail': 'Email already registered'}
 
 
 def test_read_users(client):
@@ -51,9 +51,17 @@ def test_read_users_with_users(client, user):
     assert response.json() == {'users': [user_schema]}
 
 
-def test_update_user(client, user):
+# def test_read_with_user_id(client, user):
+#     user_schema = UserPublic.model_validate(user).model_dump()
+#     response = client.get('/users/{user_schema.id}')
+#     assert response.status_code == 200
+#     assert response.json() == {'user': [user_schema]}
+
+
+def test_update_user(client, user, token):
     response = client.put(
-        '/users/1',
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
         json={
             'username': 'bob',
             'email': 'bob@example.com',
@@ -68,27 +76,45 @@ def test_update_user(client, user):
     }
 
 
-def test_update_user_inexistent(client):
+def test_update_user_inexistent(client, token):
     response = client.put(
         '/users/2',
+        headers={'Authorization': f'Bearer {token}'},
         json={
             'username': 'bob',
             'email': 'bob@example.com',
             'password': 'mynewpassword',
         },
     )
-    assert response.status_code == 404
-    assert response.json() == {'detail': 'User not found'}
+    assert response.status_code == 400
+    assert response.json() == {'detail': 'Not enough permissions'}
 
 
-def test_delete_user(client, user):
-    response = client.delete('/users/1')
+def test_delete_user(client, user, token):
+    response = client.delete(
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
     assert response.status_code == 200
     assert response.json() == {'message': 'User deleted'}
 
 
-def test_delete_user_inexistent(client):
-    response = client.delete('/users/2')
+def test_delete_user_inexistent(client, token):
+    response = client.delete(
+        '/users/2', headers={'Authorization': f'Bearer {token}'}
+    )
 
-    assert response.status_code == 404
-    assert response.json() == {'detail': 'User not found'}
+    assert response.status_code == 400
+    assert response.json() == {'detail': 'Not enough permissions'}
+
+
+def test_get_token(client, user):
+    response = client.post(
+        '/token',
+        data={'username': user.email, 'password': user.clean_password},
+    )
+    token = response.json()
+
+    assert response.status_code == 200
+    assert 'access_token' in token
+    assert 'token_type' in token
